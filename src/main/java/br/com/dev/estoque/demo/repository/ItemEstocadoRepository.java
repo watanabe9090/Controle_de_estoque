de748533de4e0e67f3b3dd380d63bc61b5ab5631
Eduardo Augusto <eduardo.rezaghi@gmail.com>
2021-05-12 13:07:15 -0300

    Criação do Item Estocado Repository

---
+++ src/main/java/br/com/dev/estoque/demo/repository/ItemEstocadoRepository.java
@@ -0,0 +1,11 @@
+package br.com.dev.estoque.demo.repository;
+
+import br.com.dev.estoque.demo.model.ItemEstocado;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemEstocadoRepository extends CrudRepository<ItemEstocado, Long> {
+
+
+}
