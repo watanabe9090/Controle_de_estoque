994217de8fcfb608dc0dc1977f3d6735e12d8611
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-05-16 15:36:50 -0300

    Tela de venda funcional, mas com problemas

--- src/main/java/br/com/dev/estoque/demo/repository/ItemEstocadoRepository.java
+++ src/main/java/br/com/dev/estoque/demo/repository/ItemEstocadoRepository.java
@@ -1,9 +1,10 @@
 package br.com.dev.estoque.demo.repository;
 
 import br.com.dev.estoque.demo.model.ItemEstocado;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemEstocadoRepository extends CrudRepository<ItemEstocado, Long> {
+public interface ItemEstocadoRepository extends JpaRepository<ItemEstocado, Long> {
 }
